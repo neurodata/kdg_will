086c48b846d9744bc33dfe19f7e9ebf43c5a2fcb
Ubuntu <ubuntu@ip-172-31-1-75.us-east-2.compute.internal>
2020-10-15 06:53:28 +0000

    updated killer fig, changed to generalization error

--- KDG.py
+++ KDG.py
@@ -9,7 +9,7 @@ class KDG(ClassifierMixin, BaseEstimator):
     def __init__(self):
         pass
     
-    def fit(self, X, y, polytope_ids):
+    def fit(self, X, y, predicted_leaf_ids_across_trees):
         #an array of all of the X-values of the class-wise means in each leaf
         self.polytope_means_X = []
 
@@ -22,27 +22,28 @@ class KDG(ClassifierMixin, BaseEstimator):
         #an array of all of the average variances of the points in each leaf corresponding to 
         #a single class from the class-wise mean in that leaf
         self.polytope_means_cov = []
-
-        for polytope_value in np.unique(polytope_ids):
-            for y_val in np.unique(y):
-                idxs_in_polytope_of_class = np.where((y == y_val) & (polytope_ids == polytope_value))[0]
-                if len(idxs_in_polytope_of_class) > 1:
-                    mean = np.mean(X[idxs_in_polytope_of_class], axis = 0)
-                    self.polytope_means_X.append(mean)
-                    #we already know the y value, so just append it 
-                    self.polytope_means_y.append(y_val)
-                    #compute the number of points in that leaf corresponding to that y value
-                    #and append to the aggregate array
-                    self.polytope_means_weight.append(len(idxs_in_polytope_of_class))
-                    #compute the distances of all the points in that leaf corresponding to that y value
-                    #from the mean X-value of the points in that leaf corresponding to that y value
-                    #compute the covariance as the average distance of the class-wise points in that leaf 
-                    #and append to the aggregate array
-                    cov = np.mean((X[idxs_in_polytope_of_class] - mean) ** 2, axis = 0)
-                    self.polytope_means_cov.append(np.eye(len(cov)) * cov)
+        
+        for polytope_ids in predicted_leaf_ids_across_trees:
+            for polytope_value in np.unique(polytope_ids, axis = 0):
+                for y_val in np.unique(y):
+                    idxs_in_polytope_of_class = np.where((y == y_val) & np.all(polytope_ids == polytope_value, axis = 1))[0]
+                    if len(idxs_in_polytope_of_class) > 1:
+                        mean = np.mean(X[idxs_in_polytope_of_class], axis = 0)
+                        self.polytope_means_X.append(mean)
+                        #we already know the y value, so just append it 
+                        self.polytope_means_y.append(y_val)
+                        #compute the number of points in that leaf corresponding to that y value
+                        #and append to the aggregate array
+                        self.polytope_means_weight.append(len(idxs_in_polytope_of_class))
+                        #compute the distances of all the points in that leaf corresponding to that y value
+                        #from the mean X-value of the points in that leaf corresponding to that y value
+                        #compute the covariance as the average distance of the class-wise points in that leaf 
+                        #and append to the aggregate array
+                        cov = np.mean((X[idxs_in_polytope_of_class] - mean) ** 2, axis = 0)
+                        self.polytope_means_cov.append(np.eye(len(cov)) * cov)
         return self
                     
-    def predict_proba(self, X):
+    def predict_proba(self, X, pooling = "polytope"):
         y_vals = np.unique(self.polytope_means_y)
         y_hat = np.zeros((len(X), len(y_vals)))
         
@@ -50,11 +51,15 @@ class KDG(ClassifierMixin, BaseEstimator):
             polytope_mean_X = self.polytope_means_X[polytope_mean_id]
             polytope_mean_y = self.polytope_means_y[polytope_mean_id]
             polytope_mean_weight = self.polytope_means_weight[polytope_mean_id]
-            covs_of_class = np.array(self.polytope_means_cov)[np.where(polytope_mean_y ==self.polytope_means_y)[0]]
-            weights_of_class = np.array(self.polytope_means_weight)[np.where(polytope_mean_y ==self.polytope_means_y)[0]]
-            polytope_mean_cov = np.average(covs_of_class, weights = weights_of_class - 1, axis = 0)
+            if pooling == "polytope":
+                covs_of_class = np.array(self.polytope_means_cov)[np.where(polytope_mean_X ==self.polytope_means_X)[0]]
+                weights_of_class = np.array(self.polytope_means_weight)[np.where(polytope_mean_X ==self.polytope_means_X)[0]]
+            else:
+                covs_of_class = np.array(self.polytope_means_cov)[np.where(polytope_mean_y ==self.polytope_means_y)[0]]
+                weights_of_class = np.array(self.polytope_means_weight)[np.where(polytope_mean_y ==self.polytope_means_y)[0]]
+            polytope_mean_cov = np.average(covs_of_class, weights = weights_of_class, axis = 0)
             var = multivariate_normal(mean=polytope_mean_X, cov=polytope_mean_cov, allow_singular=True)
-            return var.pdf(X) * polytope_mean_weight ** 2 / np.sum(weights_of_class ** 2)
+            return var.pdf(X) * polytope_mean_weight / np.sum(weights_of_class)
         
         '''  
         likelihoods = Parallel(n_jobs=10, verbose=1)(delayed(compute_pdf)(
